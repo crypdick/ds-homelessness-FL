3f35b35bb06e693041aedbeb789e34e298835a0e
Richard Decal <richard.decal@gmail.com>
2017-09-17 15:25:02 -0400

    prettifying

--- raw2clean.py
+++ raw2clean.py
@@ -28,7 +28,6 @@ with open('my_bookings_cleaned.csv', 'w') as my_bookings_cleaned_csv:
                         joined_row.extend(row)  # have not reached end of person entry. continue extending joined row.
                     else:  # we found last row for single person
                         joined_row.extend(row)
-                        # print(joined_row)
 
                         if len(joined_row) < MIN_ROW_LEN:
                             raise Exception("Not expecting rows with less than {} columns".format(MIN_ROW_LEN))
@@ -37,8 +36,7 @@ with open('my_bookings_cleaned.csv', 'w') as my_bookings_cleaned_csv:
                         joined_row = [field.strip() for field in joined_row]
                         joined_row[1:] = [field.replace(",", ".") for field in joined_row[1:]]  # exclude name field
 
-                        # joined row parsing
-
+                        ####### joined row parsing #######
                         # parse lead
                         name = joined_row[0]
                         booking_n = joined_row[1]
@@ -48,8 +46,7 @@ with open('my_bookings_cleaned.csv', 'w') as my_bookings_cleaned_csv:
                         try:
                             race, sex, ethnicity, DOB = race_sex_eth_DOB.split(" / ")
                         except ValueError:  # some people missing DOB
-                            # print("not enough values to unpack in str {} \n from joined row {}".format(race_sex_eth_DOB,
-                            #                                                                            joined_row))
+                            # print("not enough values to unpack in str {} \n from joined row {}".format(race_sex_eth_DOB, joined_row))
                             race, sex, ethnicity = race_sex_eth_DOB.split(" / ")
                             DOB = "NaT"
 
@@ -63,39 +60,32 @@ with open('my_bookings_cleaned.csv', 'w') as my_bookings_cleaned_csv:
                                 full_crimes_list.append(single_crime)
                             crimes_vec = crimes_vec[7:]  # indexes 4:7 are always blank. beyond 7 are additional crimes.
 
-                        # parse end
+                        ######## parse end #######
                         trailing_vec = joined_row[N_TRAILING_COLS:]
-                        address = trailing_vec[0].replace("ADDRESS: ","")  # combine two cols
-                        # address = address[9:]  # strip "ADDRESS: "
-                        place_of_birth = "{}.{}".format(trailing_vec[1], trailing_vec[2]).replace("POB: ","")
-                        # place_of_birth = place_of_birth[5:]  # strip "POB: "
-                        releaseDate = trailing_vec[7].replace("RELEASE DATE: ","")
-                        # releaseDate = releaseDate[14:]  # strip "RELEASE DATE: "
-                        releaseCode = trailing_vec[8].replace("RELEASE CODE: ","")
-                        # releaseCode = releaseCode[14:]  # strip "RELEASE CODE: "
-                        SOID = trailing_vec[9].replace("SOID: ","")
-                        # SOID = SOID[6:]  # strip "SOID: "
-                        # print(address,place_of_birth,releaseDate,releaseCode,SOID)
+                        address = trailing_vec[0].replace("ADDRESS:","")  # combine two cols
+                        place_of_birth = "{}{}".format(trailing_vec[1], trailing_vec[2]).replace("POB:","")
+                        releaseDate = trailing_vec[7].replace("RELEASE DATE:","")
+                        releaseCode = trailing_vec[8].replace("RELEASE CODE:","")
+                        SOID = trailing_vec[9].replace("SOID:","")
 
 
                         # avoid index errors if full_crimes_list is empty
                         if not full_crimes_list:  # returns False if empty
                             full_crimes_list.append(['', '', '', ''])  # insert placeholders
 
-                        # storing
+                        ######## storing #######
                         clean_booking_vec = [name, booking_n, agency, ABN, race, sex, ethnicity, DOB] + \
                                             full_crimes_list[0] + [address, place_of_birth, releaseDate, releaseCode,
                                                                    SOID]
-
-                        # print(clean_booking_vec)
+                        clean_booking_vec = [field.strip() for field in clean_booking_vec]
                         writer.writerow(clean_booking_vec)
 
                         # if addl crimes, store them in separate file
                         if len(full_crimes_list) > 1:
                             addl_charges = full_crimes_list[1:]
                             for charge in addl_charges:
-                                clean_addl_booking = [booking_n] + charge
+                                clean_addl_booking = [booking_n] + charge  # Dr gillman's clean file has booking#
                                 addl_writer.writerow(clean_addl_booking)
 
-                        # clear joined row for next person
+                        # clear for next booking
                         joined_row = []
